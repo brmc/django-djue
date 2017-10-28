86c9b5692a4cb75a3eb44aa71c15c8d0f1376c0d
Brian <brian.mcclr@gmail.com>
2017-10-28 11:23:15 +0200

    Added support for multiple components in a view

--- djue/vue/views.py
+++ djue/vue/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from djue.utils import render_to_js_string
+from djue.utils import render_to_js_string, convert_to_kebab_case
 from djue.vue.core import SingleFileComponent
 
 
@@ -14,9 +14,10 @@ class View(SingleFileComponent):
         super().__init__(*args, **kwargs)
 
     def render(self):
+        tags = {convert_to_kebab_case(c.name) for c in self.components}
         names = {c.name for c in self.components}
         imports = {x.relative_module_import_string for x in self.components}
-        html = '\n'.join([f'<{name}></{name}>' for name in names])
+        html = '\n'.join([f'<{tag}></{tag}>' for tag in tags])
 
         js = render_to_js_string('djue/view.js',
                                  {'names': names,
