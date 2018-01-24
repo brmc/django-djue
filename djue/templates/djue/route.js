c603f687b3049821291d8a4d42c9febb9b372d21
Brian <brian.mcclr@gmail.com>
2018-01-24 06:42:26 +0100

    Purged empty routes from router

--- djue/templates/djue/route.js
+++ djue/templates/djue/route.js
@@ -1,8 +1,8 @@
-  {
+{% if route.view or route.url == '/' %}  {
     path: '{{route.url}}',{% if route.view %}
-    component: {{route.view.name}},{% elif route.url == '/' %}component: Home,{% endif %}{% if route.name %}
+    component: {{route.view.name}},{% else %}component: Home,{% endif %}{% if route.name %}
     name: '{{ route.lookup_name }}', {% endif %}{% if route.children %}
     children: [{% for child in route.children %}
       {% include 'djue/route.js' with route=child %},{% endfor %}
     ]{% endif %}
-  }
+  } {% else %}{% for child in route.children %}{% include 'djue/route.js' with route=child %}{% if not forloop.last %},{% endif %}{% endfor %}{% endif %}
