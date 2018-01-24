0b7322cf217a3e488e34a70dc211d268f1b084fe
Brian <brian.mcclr@gmail.com>
2018-01-24 05:41:47 +0100

    Added Home view to root path if no component is defined
    It seems vue doesn't calculate nested paths if a path is
    missing for more than 1 level of the route structure.
    This may have been changed in newer versions of vue, but
    for now, I'm just going to do this

--- djue/templates/djue/route.js
+++ djue/templates/djue/route.js
@@ -1,6 +1,6 @@
   {
     path: '{{route.url}}',{% if route.view %}
-    component: {{route.view.name}},{% endif %}{% if route.name %}
+    component: {{route.view.name}},{% elif route.url == '/' %}component: Home,{% endif %}{% if route.name %}
     name: '{{ route.lookup_name }}', {% endif %}{% if route.children %}
     children: [{% for child in route.children %}
       {% include 'djue/route.js' with route=child %},{% endfor %}
