c22c0a57be2e8c83bed319199c4b697332ec7cdf
Brian <brian.mcclr@gmail.com>
2018-03-24 13:37:54 +0100

    modified destroy to match expected blah blah

---
+++ djue/templates/djue/actions/destroy.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+import DestroyViewSetComponent from '../../../components/DestroyViewSetComponent'
+
+export default Vue.extend({
+  mixins: [DestroyViewSetComponent],
+  data () {
+    return {
+      namespace: '{{ self.app }}/{{ self.model }}',
+      routeName: '{{ self.model|lower }}-detail',
+    }
+  },
+  computed: {
+    ...mapState('{{ self.app }}/{{ self.model }}', {
+      object: function (state) {
+        return state.objects.local[this.$route.params.pk]
+      }
+    }),
+  },
+})
