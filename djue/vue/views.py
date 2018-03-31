4886d7af338a3fe1b29e7499beac374f2719ad23
Brian <brian.mcclr@gmail.com>
2018-03-31 17:25:28 +0200

    Created views for module and core

--- djue/vue/views.py
+++ djue/vue/views.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from djue import SCFMixin, CoreFile, ModuleFile
 from djue.utils import render_to_js_string, convert_to_kebab_case, \
     render_to_html_string
 from djue.vue.core import SingleFileComponent
@@ -24,3 +24,23 @@ class View(SingleFileComponent):
                                   'imports': imports})
 
         return self.render_sfc(html, js)
+
+
+class CoreView(SCFMixin, CoreFile):
+    dir: str = 'views'
+    file_ext: str = '.vue'
+    path_format: str = 'djue/core/views/{}'
+
+    def __init__(self, view, name):
+        super().__init__(name)
+        self.set_templates_from_path_format(self.path_format, [view])
+
+
+class ModuleView(SCFMixin, ModuleFile):
+    dir: str = 'views'
+    file_ext: str = '.vue'
+    path_format: str = 'djue/modules/views/{}'
+
+    def __init__(self, view, app, name):
+        super().__init__(app, name)
+        self.set_templates_from_path_format(self.path_format, [view])
