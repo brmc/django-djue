4efa18f8a06eb82c64719e13c4aec467e056df6c
Brian <brian.mcclr@gmail.com>
2018-03-31 17:18:38 +0200

    Forgot to commit changes to writer

---
+++ djue/vue/writers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from djue import get_output_path, log
+
+
+class SimpleWriter:
+    def __init__(self, relative_path):
+        path = get_output_path()
+        self.path = os.path.join(path, relative_path)
+
+    def write(self, payload):
+        file_path = self.path
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'w+') as file:
+            log('writing to ' + file_path)
+            file.write(payload)
